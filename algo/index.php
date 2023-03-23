95a4c1051630e513bd72984709bb94ceb60a0eb3
wangli <wanglijtm@gmail.com>
2023-03-23 11:21:07 +0100

    mis à jour de cours

--- algo/index.php
+++ algo/index.php
@@ -1,10 +1,55 @@
 <?php
-$nombre1=3;
-$nombre2=5;
 
-//exponentiation
-echo $nombre1 ** $nombre2 ;
+    $nombre1 = 3;
+    $nombre2 = 5;
 
-$nombre1 = $nombre2 + 12;
-echo $nombre1
+    // Addition
+    echo $nombre1 + $nombre2 . "\n";
+    
+    
+    // Soustraction
+    echo $nombre1 - $nombre2 . "\n";
+    
+    
+    // Multiplication
+    echo $nombre1 * $nombre2 . "\n";
+    
+    
+    // Division
+    echo $nombre1 / $nombre2 . "\n";
+    
+    
+    // Modulo
+    echo $nombre1 % $nombre2 . "\n";
+    
+
+    // Exponentiation
+    echo $nombre1 ** $nombre2 . "\n";
+
+
+    // L'incrémentation
+    // $nombre1++;
+    // echo $nombre1 . "\n";
+    
+    
+    // $nombre1 += 1;
+    // $nombre1 += 10;
+    // echo $nombre1 . "\n";
+
+
+    // $nombre1 = $nombre1 + 12;
+    // echo $nombre1;
+
+
+    // Décrémentation
+    // $nombre2--;
+    // echo $nombre2;
+
+    // $nombre2 -= 1;
+    // $nombre2 -= 10;
+    // echo $nombre2 . "\n";
+
+
+    // $nombre2 = $nombre2 + 12;
+    // echo $nombre2;
 ?>
